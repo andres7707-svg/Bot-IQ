4efcd7a16f4e7eba34b34636f25e9df947325755
andres7707-svg <andres7707@gmail.com>
2025-10-05 00:34:54 -0500

    Update hook-iqoptionapi.py
    Add hook for iqoptionapi (PyInstaller)

--- hook-iqoptionapi.py
+++ hook-iqoptionapi.py
@@ -1,2 +1,5 @@
+# hook-iqoptionapi.py
 from PyInstaller.utils.hooks import collect_all
+
+# Tell PyInstaller to collect everything from the iqoptionapi package
 datas, binaries, hiddenimports = collect_all('iqoptionapi')
